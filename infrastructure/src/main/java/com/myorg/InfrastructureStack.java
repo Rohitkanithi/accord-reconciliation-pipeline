8ffffe4e78cccab8a08c99fda2a70cfea9c0a11f
Rohitkanithi <63586900+Rohitkanithi@users.noreply.github.com>
2025-06-25 22:50:51 -0500

    feat(infra): Add SQS queue for VerificationService

--- infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -1,11 +1,13 @@
 package com.myorg;
 
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.s3.Bucket;
 import software.amazon.awscdk.services.s3.EventType;
 import software.amazon.awscdk.services.s3.notifications.SqsDestination;
 import software.amazon.awscdk.services.sns.Topic;
+import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.ssm.StringParameter;
 import software.constructs.Construct;
@@ -14,27 +16,26 @@ public class InfrastructureStack extends Stack {
     public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // 1. SQS Queue for Ingestion
+
         Queue intakeQueue = Queue.Builder.create(this, "AccordIntakeQueue")
                 .queueName("accord-intake-queue")
                 .build();
 
-        // 2. S3 Bucket for Document Uploads
-        Bucket evidenceBucket = Bucket.Builder.create(this, "AccordEvidenceBucket").build();
+        Bucket evidenceBucket = Bucket.Builder.create(this, "AccordEvidenceBucket")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .autoDeleteObjects(true)
+                .build();
 
-        // 3. S3 Event Notification to SQS
         evidenceBucket.addEventNotification(
                 EventType.OBJECT_CREATED,
                 new SqsDestination(intakeQueue),
                 software.amazon.awscdk.services.s3.NotificationKeyFilter.builder().suffix(".pdf").build()
         );
 
-        // 4. SNS Topic for TransactionReceived Events
         Topic transactionReceivedTopic = Topic.Builder.create(this, "TransactionReceivedTopic")
                 .topicName("accord-transaction-received-topic")
                 .build();
 
-        // 5. SSM Parameters to store resource names for our microservices
         StringParameter.Builder.create(this, "SqsQueueNameParameter")
                 .parameterName("/accord/queues/intake-queue-name")
                 .stringValue(intakeQueue.getQueueName())
@@ -43,6 +44,22 @@ public class InfrastructureStack extends Stack {
         StringParameter.Builder.create(this, "SnsTopicArnParameter")
                 .parameterName("/accord/topics/transaction-received-arn")
                 .stringValue(transactionReceivedTopic.getTopicArn())
+                .build();
+
+        // VERIFICATION SERVICE ---
+
+        // 1. Create a dedicated SQS queue for the Verification service
+        Queue verificationQueue = Queue.Builder.create(this, "VerificationQueue")
+                .queueName("verification-queue")
+                .build();
+
+        // 2. Subscribe this new queue to the existing SNS topic.
+        transactionReceivedTopic.addSubscription(new SqsSubscription(verificationQueue));
+
+        // 3. Store the new queue's URL in SSM Parameter Store for the new service to use.
+        StringParameter.Builder.create(this, "VerificationQueueUrlParameter")
+                .parameterName("/accord/queues/verification-queue-url")
+                .stringValue(verificationQueue.getQueueUrl())
                 .build();
     }
 }
